4bf4539e970e9b0aaa48311fde5a931b9ab01e38
=sandeepnegi1996 <9041128179sandy@gmail.com>
2019-09-14 12:25:53 +0530

    this is initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const log= require('./logger')
+
+console.log("hello world");
+
+log("this is log message");
+
+
---
+++ logger.js
@@ -0,0 +1,19 @@
+
+const url="www.logger.com/log"
+
+console.log(__filename);
+console.log(__dirname);
+function log(message) {
+    //send the http request with this message
+
+
+
+
+
+    console.log(message);
+    
+}
+
+
+
+module.exports=log;
